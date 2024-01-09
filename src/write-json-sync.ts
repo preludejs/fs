45e34d736771af3526e0a9535d6dd271997551ea
Mirek Rusin <mirek@me.com>
2024-01-09 22:23:09 +0100

    feat!: append new line when writing json

--- src/write-json-sync.ts
+++ src/write-json-sync.ts
@@ -1,13 +1,20 @@
 import type * as Fs from 'fs'
 import writeStringSync from './write-string-sync.js'
 
+type Replacer =
+  Parameters<typeof JSON.stringify>[1]
+
+type Space =
+  Parameters<typeof JSON.stringify>[2]
+
 const writeJsonSync =
   (
     path: Fs.PathLike,
     value: unknown,
-    replacer: Parameters<typeof JSON.stringify>[1] = null,
-    space: Parameters<typeof JSON.stringify>[2] = 2
+    replacer: Replacer = null,
+    space: Space = 2,
+    nl = true
   ) =>
-    writeStringSync(path, JSON.stringify(value, replacer, space))
+    writeStringSync(path, JSON.stringify(value, replacer, space) + (nl ? '\n' : ''))
 
 export default writeJsonSync
--- src/write-json.ts
+++ src/write-json.ts
@@ -1,13 +1,20 @@
 import type * as Fs from 'fs'
 import writeString from './write-string.js'
 
+type Replacer =
+  Parameters<typeof JSON.stringify>[1]
+
+type Space =
+  Parameters<typeof JSON.stringify>[2]
+
 const writeJson =
   (
     path: Fs.PathLike,
     value: unknown,
-    replacer: Parameters<typeof JSON.stringify>[1] = null,
-    space: Parameters<typeof JSON.stringify>[2] = 2
+    replacer: Replacer = null,
+    space: Space = 2,
+    nl = true
   ) =>
-    writeString(path, JSON.stringify(value, replacer, space))
+    writeString(path, JSON.stringify(value, replacer, space) + (nl ? '\n' : ''))
 
 export default writeJson
